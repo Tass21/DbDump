d4278b9f3e712e10618dc40befd3be43e30232a6
dev_kyn <wizardkyn@naver.com>
2016-10-22 21:04:37 +0900

    add @Override

--- src/main/java/com/bcg/dbdump/CountService.java
+++ src/main/java/com/bcg/dbdump/CountService.java
@@ -10,20 +10,24 @@ public class CountService implements Daemon {
 	private final static Logger logger = Logger.getLogger(CountService.class);
 	private Thread countThread = null;
 	private static final CountService CountService = new CountService();
-	
+
+	@Override
 	public void destroy() {
 		logger.debug("Service Destroy");
 	}
 
+	@Override
 	public void init(DaemonContext arg0) throws DaemonInitException, Exception {
 		countThread = new CountThread();
 	}
 		
+	@Override
 	public void start() throws Exception {
 		countThread.start();
 		logger.debug("Service Start");
 	}
 
+	@Override
 	public void stop() throws Exception {
 		countThread.interrupt();
 		destroy();
